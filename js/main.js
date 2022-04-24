8713491bafc264b7cbdcf76e38ea77d449637bae
Triton-Halley <Triton.halley@outlook.com>
2022-04-24 17:17:19 +0430

    map loaded correctly library working

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
+"use strict";
 
+const MapSection = document.querySelector(".Section-1");
+const coords = [];
+// my coords
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log(latitude, longitude);
+    coords.push(latitude);
+    coords.push(longitude);
+    loadMap();
+  },
+  function () {
+    console.log(`I don't have Access  `);
+  }
+);
+
+const loadMap = function () {
+  const map = L.map("map", { zoomControl: false }).setView(coords, 13);
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  L.marker(coords)
+    .addTo(map)
+    .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+    .openPopup();
+
+  new L.Control.Zoom({ position: "topright" }).addTo(map);
+  // L.Control.Zoom()
+};
