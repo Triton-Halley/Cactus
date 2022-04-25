7155eb3fa819b0d7dfca55b2b340be5ed7dfa7c2
Triton-Halley <Triton.halley@outlook.com>
2022-04-25 13:45:24 +0430

    add some icons forms and workouts exampls now I want to implement OOP Architecture

--- js/main.js
+++ js/main.js
@@ -1,26 +1,28 @@
 "use strict";
 
 const MapSection = document.querySelector(".Section-1");
-const coords = [];
+const RegisterForm = document.querySelector(".Register-Workout");
+const coords = [35.782706207126395, 51.43842669079781];
+
 // my coords
-navigator.geolocation.getCurrentPosition(
-  function (position) {
-    const { latitude } = position.coords;
-    const { longitude } = position.coords;
-    console.log(latitude, longitude);
-    coords.push(latitude);
-    coords.push(longitude);
-    loadMap();
-  },
-  function () {
-    console.log(`I don't have Access  `);
-  }
-);
+// navigator.geolocation.getCurrentPosition(
+//   function (position) {
+//     const { latitude } = position.coords;
+//     const { longitude } = position.coords;
+//     console.log(latitude, longitude);
+//     coords.push(latitude);
+//     coords.push(longitude);
+//     loadMap();
+//   },
+//   function () {
+//     console.log(`I don't have Access  `);
+//   }
+// );
 
 const loadMap = function () {
   const map = L.map("map", { zoomControl: false }).setView(coords, 13);
 
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(map);
@@ -32,4 +34,17 @@ const loadMap = function () {
 
   new L.Control.Zoom({ position: "topright" }).addTo(map);
   // L.Control.Zoom()
+
+  map.on("click", function (e) {
+    //console.log(e);
+    RegisterForm.classList.remove("hidden");
+    addNewWorkout(e, map);
+  });
+};
+
+loadMap();
+
+const addNewWorkout = function (e, _map) {
+  const { lat, lng } = e.latlng;
+  L.marker([lat, lng]).addTo(_map);
 };
